a7358f8bbe88c9d05d25c4d0169a2a9a73a72151
Greg Gerard <foreignbyproxy@users.noreply.github.com>
2022-09-27 12:03:08 -0400

    Adjust Button with correct props

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-			<BlockButton>Web Button</BlockButton>
+			<BlockButton size={"md"}>Web Button</BlockButton>
     </div>
   );
 }
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
-import { Button } from "@chakra-ui/react";
+import { Button, ButtonProps } from "@chakra-ui/react";
 
-export const BlockButton = ({ children }) => {
-  return <Button>{children}</Button>
+export const BlockButton: React.FC<ButtonProps> = ({ children, ...rest }) => {
+  return <Button {...rest}>{children}</Button>;
 };
