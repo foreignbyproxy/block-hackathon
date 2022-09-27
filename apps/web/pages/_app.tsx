1e740b55cdaaf5804ecc8ac69bfa3041acf107cb
Greg Gerard <foreignbyproxy@users.noreply.github.com>
2022-09-27 11:53:27 -0400

    Fix type errors and move theme to UI

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,50 +1,9 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "ui";
 
-import { colors } from "ui";
+import type { AppProps } from 'next/app'
 
-const theme = extendTheme({
-  colors,
-  components: {
-    Button: {
-      // 1. We can update the base styles
-      baseStyle: {
-        fontWeight: "bold", // Normally, it is "semibold"
-      },
-      // 2. We can add a new button size or extend existing
-      sizes: {
-        xl: {
-          h: "56px",
-          fontSize: "lg",
-          px: "32px",
-        },
-      },
-      // 3. We can add a new visual variant
-      variants: {
-        "with-shadow": {
-          bg: "red.400",
-          boxShadow: "0 0 2px 2px #efdfde",
-        },
-        // 4. We can override existing variants
-        solid: (props) => ({
-          bg: props.colorMode === "dark" ? "red.300" : "red.500",
-        }),
-        // 5. We can add responsive variants
-        sm: {
-          bg: "red.500",
-          fontSize: "md",
-        },
-      },
-      // 6. We can overwrite defaultProps
-      defaultProps: {
-        size: "lg", // default is md
-        variant: "sm", // default is solid
-        colorScheme: "green", // default is gray
-      },
-    },
-  },
-});
-
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
--- packages/ui/theme.ts
+++ packages/ui/theme.ts
@@ -1,7 +1,57 @@
+import type { StyleFunctionProps } from "@chakra-ui/styled-system";
+import { extendTheme } from "@chakra-ui/react";
+
 export const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
+  orange: {
+    100: "#FFEDE7",
+    200: "#FFD2C3",
+    300: "#FF9E81",
+    400: "#FF7C55",
+    500: "#E56740",
+    600: "#CC5936",
+  }
+};
+
+export const components = {
+  Button: {
+    // 1. We can update the base styles
+    baseStyle: {
+      fontWeight: "bold", // Normally, it is "semibold"
+    },
+    // 2. We can add a new button size or extend existing
+    sizes: {
+      xl: {
+        h: "56px",
+        fontSize: "lg",
+        px: "32px",
+      },
+    },
+    // 3. We can add a new visual variant
+    variants: {
+      "with-shadow": {
+        bg: "orange.400",
+        boxShadow: "0 0 2px 2px #efdfde",
+      },
+      // 4. We can override existing variants
+      solid: (props: StyleFunctionProps) => ({
+        bg: props.colorMode === "dark" ? "orange.400" : "orange.400",
+      }),
+      // 5. We can add responsive variants
+      sm: {
+        bg: "orange.400",
+        fontSize: "md",
+      },
+    },
+    // 6. We can overwrite defaultProps
+    defaultProps: {
+      size: "lg", // default is md
+      variant: "sm", // default is solid
+      colorScheme: "orange", // default is gray
+    },
   },
-}
+};
+
+export const theme = extendTheme({
+  colors,
+  components,
+});
