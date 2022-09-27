c9fb3d8edbc66050ecf2daac16f2de040457f988
Greg Gerard <foreignbyproxy@users.noreply.github.com>
2022-09-27 18:22:27 -0400

    Add sections to web

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,17 +1,46 @@
-import { Button, Select } from "ui";
+import { Box, Text, HStack } from "@chakra-ui/react";
+import { Button, Container, StackedCTA } from "ui";
+import Image from "next/future/image";
+
+import ImageCity from '../assets/images/city.jpeg';
+import ImagePot from '../assets/images/pot.jpeg';
+import ImageSwirl from '../assets/images/swirl.jpeg';
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-			<Button size={"md"}>Web Button</Button>
-			<Button size={"lg"}>Web Button</Button>
-      <h1>Select</h1>
-      <Select size={"md"} placeholder="Select an option" variant="block_orange">
-        <option value='This is cool'>This is cool</option>
-        <option value='This is ok'>This is ok</option>
-        <option value='This is bad'>This is bad</option>
-      </Select>
-    </div>
+    <>
+      <Container backgroundImage="https://picsum.photos/1920/700" display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="xl">
+        <Container
+          variant="borderless"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          gap="6"
+          bgColor="slate.200"
+          borderRadius="lg"
+        >
+          <Text as="h1">Welcome to Block Hackathon Demo</Text>
+          <Button>Learn More</Button>
+        </Container>
+      </Container>
+      <Container px="8" py="12">
+        <Text as="h1" mb="4" align="center">Welcome to Block Hackathon Demo</Text>
+        <HStack spacing="8" justifyContent="center">
+          <StackedCTA>
+            <Image src={ImageCity} alt="Learn more" />
+            <Text>This is the best tool to estimate your renovation</Text>
+          </StackedCTA>
+          <StackedCTA>
+            <Image src={ImagePot} alt="Learn more" />
+            <Text>This is the best tool to estimate your renovation</Text>
+          </StackedCTA>
+					<StackedCTA>
+            <Image src={ImageSwirl} alt="Learn more" />
+            <Text>This is the best tool to estimate your renovation</Text>
+          </StackedCTA>
+        </HStack>
+      </Container>
+    </>
   );
 }
